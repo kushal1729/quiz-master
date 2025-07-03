7e201761adc4387851a636f68e1b73585e06d193
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-07-03 15:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://new_username:new_password@localhost/quiz_db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'quiz.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
