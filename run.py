f0d9a609ef3a4d36f9b10fcbb9850713ac8520f4
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-07-12 14:52:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()  # 👈 This creates all tables if they don't exist
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
