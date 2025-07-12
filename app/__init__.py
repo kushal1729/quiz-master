aefb5f407269f9e0a3f21b49bb8bf998e1534884
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-07-12 18:23:41 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,15 @@ from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
     db.init_app(app)
     login_manager.init_app(app)
 
-    from app.routes import main
+    from .routes import main
     app.register_blueprint(main)
-
     return app
