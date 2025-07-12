ec5538d093fcdcec69159fee4858faa26b46c832
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-07-12 18:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'quiz.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key')
