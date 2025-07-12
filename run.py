c36cefe65e37c421d7f2979e0217aa06f6bd309c
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-07-12 14:54:51 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,20 @@ from app import create_app, db
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()  # 👈 This creates all tables if they don't exist
+from app.models import User
+
+# Check if admin exists
+if not User.query.filter_by(username='admin').first():
+    admin_user = User(
+        username='admin',
+        password='admin123',
+        full_name='Admin User',
+        qualification='Admin',
+        dob='2005-04-15',
+        is_admin=True
+    )
+    db.session.add(admin_user)
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
