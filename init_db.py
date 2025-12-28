14d3ed7fd23edb19ac26f678a897a6e520e887e1
kushal1729 <144520925+kushal1729@users.noreply.github.com>
2025-12-29 01:40:03 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,1 +1,17 @@
+from app import create_app, db
+from app.models import User
 
+app = create_app()
+with app.app_context():
+    
+    admin_user = User(
+        username='admin',
+        password='admin123',
+        full_name='Admin User',
+        qualification='Admin',
+        dob='2005-04-15',
+        is_admin=True  
+    )
+    db.session.add(admin_user)
+    db.session.commit()
+    print("Admin user created successfully!")
